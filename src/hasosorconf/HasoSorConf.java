d00fc0d6e269772d464d04a8e886db9af086ae9c
virmoc <virmoc411@gmail.com>
2022-03-11 15:12:51 +0100

    1. név megadása

--- src/hasosorconf/HasoSorConf.java
+++ src/hasosorconf/HasoSorConf.java
@@ -2,7 +2,7 @@
 package hasosorconf;
 public class HasoSorConf {
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "TV";
         String nev2; 
         String nev3;
     }
